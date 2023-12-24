27872e735f6ffb92d71d243ab7985d20f45906cd
David Aparicio Sanz <108094535+davidapariciosanz@users.noreply.github.com>
2023-12-24 03:41:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,3 +17,42 @@ with st.expander("Show code...",expanded=False):
             "hg.streamlit_highcharts(chartDef,640)"
             ,language="python")
 
+
+# Set the title of the app
+st.title("My Streamlit App")
+
+chart_def = {
+    "title": {
+        "text": "Sales of petroleum products March, Norway",
+        "align": "left"
+    },
+    "xAxis": {
+        "categories": ["Jet fuel", "Duty-free diesel"]
+    },
+    "yAxis": {
+        "title": {"text": "Million liter"}
+    },
+    "series": [
+        {"type": "column",
+         "name": "2020",
+         "data": [59, 83]},
+        {"type": "column",
+         "name": "2021",
+         "data": [24, 79]
+         },
+        {"type": "column",
+         "name": "2022",
+         "data": [58, 88]
+         },
+        {"type": "spline",
+         "name": "Average",
+         "data": [47, 83.33],
+         "marker": {
+             "lineWidth": 2,
+             "fillColor": "black",
+         }
+         }
+    ]
+}
+
+hg.streamlit_highcharts(chart_def, 640)
