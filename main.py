0a0f00ff8f8c6e4069ef4dafdb58e64208474130
David Aparicio Sanz <108094535+davidapariciosanz@users.noreply.github.com>
2023-12-30 00:00:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -99,3 +99,121 @@ as_str = {
 }
 
 hg.streamlit_highcharts(as_str, 640)
+
+
+plot_2 = {
+   "chart":{
+      "type": "boxplot"
+   },
+   "title":{
+      "text": "Highcharts Box Plot Example"
+   },
+   "legend":{
+      "enabled":False
+   },
+   "xAxis":{
+      "categories":[
+         "1",
+         "2",
+         "3",
+         "4",
+         "5"
+      ],
+      "title":{
+         "text":"Experiment No."
+      }
+   },
+   "yAxis":{
+      "title":{
+         "text":"Observations"
+      },
+      "plotLines":[
+         {
+            "value":932,
+            "color":"red",
+            "width":1,
+            "label":{
+               "text":"Theoretical mean: 932",
+               "align":"center",
+               "style":{
+                  "color":"gray"
+               }
+            }
+         }
+      ]
+   },
+   "series":[
+      {
+         "name":"Observations",
+         "data":[
+            [
+               760,
+               801,
+               848,
+               895,
+               965
+            ],
+            [
+               733,
+               853,
+               939,
+               980,
+               1080
+            ],
+            [
+               714,
+               762,
+               817,
+               870,
+               918
+            ],
+            [
+               724,
+               802,
+               806,
+               871,
+               950
+            ],
+            [
+               834,
+               836,
+               864,
+               882,
+               910
+            ]
+         ]
+      },
+      {
+         "name":"Outliers",
+         "color":"black",
+         "type":"scatter",
+         "data":[
+            [
+               0,
+               644
+            ],
+            [
+               4,
+               718
+            ],
+            [
+               4,
+               951
+            ],
+            [
+               4,
+               969
+            ]
+         ],
+         "marker":{
+            "fillColor":"white",
+            "lineWidth":1,
+            "lineColor":"black"
+         }
+      }
+   ]
+}
+
+hg.streamlit_highcharts(plot_2, 640)
+
+         
